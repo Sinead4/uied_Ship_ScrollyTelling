d6513b90f069f351b79a5f27f89d84d2ba81962c
marcgutzwiller <62597982+marcgutzwiller@users.noreply.github.com>
2021-11-12 20:17:18 +0100

    Add title "Die Brigantine" add sky

---
+++ Assets/Scripts/TitleText.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleText : MonoBehaviour
+{
+    private Text title;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        title = GetComponent<Text>();
+        title.text = "Die Brigantine";
+        title.fontStyle = FontStyle.Bold;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

68357f0bd62050e41534c7d791c6af5aaaf8bc9b
sinead4 <sinead.ganz@students.fhnw.ch>
2021-11-16 14:54:46 +0100

    entry scene finished

--- Assets/Scripts/ScrollTest.cs
+++ Assets/Scripts/ScrollTest.cs
@@ -8,10 +8,13 @@ public class ScrollTest : MonoBehaviour
     // Start is called before the first frame update
     public GameObject shipObject;
     private Vector3 currentShipPos;
+    private Vector3 currentTitlePos;
+    public GameObject title;
    
     void Start()
     {
         currentShipPos = shipObject.transform.position;
+        currentTitlePos = title.transform.position;
     }
 
     // Update is called once per frame
@@ -20,9 +23,12 @@ public class ScrollTest : MonoBehaviour
        
         if (Input.mouseScrollDelta.y == -1)
         {
-            Vector3 newPos = new Vector3(14, currentShipPos.y, currentShipPos.z);
-            shipObject.transform.position = Vector3.MoveTowards(shipObject.transform.position, newPos, 1.5f);
-   
+            Vector3 newPosShip = new Vector3(14, currentShipPos.y, currentShipPos.z);
+            shipObject.transform.position = Vector3.MoveTowards(shipObject.transform.position, newPosShip, 1.5f);
+            
+            Vector3 newPosText = new Vector3(769, currentTitlePos.y, currentTitlePos.z);
+            title.transform.position = Vector3.MoveTowards(title.transform.position, newPosText, 1.8f);
+
         }
   
     }

865da1f23ad872d53ca90e9abb1d8a2eb8eea261
sinead4 <sinead.ganz@students.fhnw.ch>
2021-11-23 15:32:53 +0100

    added script that camera follows a path and rotates

---
+++ Assets/Scripts/MainClass.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class MainClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // public Ship newShip;
+    private Vector3 startShipPos;
+    private Vector3 startTitlePos;
+    public GameObject title;
+    public GameObject shipObj;
+   
+    void Start()
+    {
+        // newShip = gameObject.AddComponent<Ship>();
+            // new Ship(new Vector3(-165, 3.6f, 60));
+        // startShipPos = this.transform.position;
+        startTitlePos = title.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.mouseScrollDelta.y == -1)
+        {
+            Vector3 newPosText = new Vector3(769, startTitlePos.y, startTitlePos.z);
+            title.transform.position = Vector3.MoveTowards(title.transform.position, newPosText, 1.8f);
+            
+            Vector3 newPos = new Vector3(14, shipObj.transform.position.y, shipObj.transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, newPos, 1.4f);
+            
+        }
+
+        // if (Input.mouseScrollDelta.y == -1)
+        // {
+        //     Debug.Log("xPos: " + newShip.transform.position.x);
+        //     Debug.Log("yPos: " + newShip.transform.position.y);
+        //     Debug.Log("zPos: " + newShip.transform.position.z);
+        //
+        //     if (newShip.transform.position.x <= 14)
+        //     {
+        //         newShip.MoveShipTowardsAPoint(14, newShip.transform.position.y, newShip.transform.position.z, 1.5f);
+        //        
+        //         Vector3 newPosText = new Vector3(769, startTitlePos.y, startTitlePos.z);
+        //         title.transform.position = Vector3.MoveTowards(title.transform.position, newPosText, 1.8f);
+        //     }
+        //
+        //     if (newShip.transform.position.x == 14)
+        //     {
+        //         newShip.transform.Rotate(0,0,0);
+        //     }
+            
+         }
+  
+    }
+
+
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Ship: MonoBehaviour
+    {
+        
+        public float x;
+        public float y;
+        public float z;
+
+        public Ship(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+          
+            transform.position = new Vector3(x, y, z);
+        }
+        
+        public void MoveShipTowardsAPoint( float x, float y, float z, float speed)
+        {
+            Vector3 newPos = new Vector3(x, y, z);
+            transform.position = Vector3.MoveTowards(transform.position, newPos, speed);
+            
+        }
+        
+    }
+}

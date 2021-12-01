04c192ee977c1e92ace38d0717ce2cc6b66e59e1
marcgutzwiller <62597982+marcgutzwiller@users.noreply.github.com>
2021-12-01 10:47:23 +0100

    Install Cinemachine

---
+++ Assets/Scripts/DollyCamPathFollower.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Cinemachine;
+namespace DefaultNamespace
+{
+    public class DollyCamPathFollower : MonoBehaviour
+    {
+        public float speed = 2f;
+        private float distanceTraveled;
+    
+        public CinemachineVirtualCamera currentCamera;
+        public CinemachineTrackedDolly cinemachineTrackedDolly;
+    
+        
+        void FixedUpdate()
+        {
+            distanceTraveled += speed * Input.mouseScrollDelta.y;
+        
+            cinemachineTrackedDolly = currentCamera.GetCinemachineComponent<CinemachineTrackedDolly> ();
+            cinemachineTrackedDolly.m_PathPosition = distanceTraveled;
+        }
+    }
+    
+}

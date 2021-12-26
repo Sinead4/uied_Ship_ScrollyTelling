8858d2b6d8c7f9c9dfb00a60fb78b4e6f6d1bfa1
sinead4 <sinead.ganz@students.fhnw.ch>
2021-12-26 14:17:06 +0100

    sails pulsing implemented

--- Assets/Scripts/SailHandling.cs
+++ Assets/Scripts/SailHandling.cs
@@ -9,6 +9,15 @@ public class SailHandling : MonoBehaviour
     public GameObject sailMiddleBack;
     public Shader OutlineShader;
 
+    //Pulse 
+    public float growthBound;
+    public float shrinkBound;
+    public float currentRatio;
+    public float approachSpeed;
+    private Coroutine routine;
+    private bool isPulsing = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,13 +36,53 @@ public class SailHandling : MonoBehaviour
             sailFrontAndBack.GetComponent<MeshRenderer>().material.shader = OutlineShader;
             sailMiddleFront.GetComponent<MeshRenderer>().material.shader = OutlineShader;
             sailMiddleBack.GetComponent<MeshRenderer>().material.shader = OutlineShader;
-
-
-         }
+            if (!isPulsing)
+            {
+                this.routine = StartCoroutine(this.Pulse());
+            }
+            
+        }
+        else
+        {
+           // Shader wieder wegnehmen
+        }
     }
 
-    private void sailClicked()
+    private IEnumerator Pulse()
     {
+        isPulsing = true;
 
+        while (this.currentRatio != this.growthBound)
+        {
+            // Determine the new ratio to use
+            currentRatio = Mathf.MoveTowards(currentRatio, growthBound, approachSpeed);
+
+            // Update element
+            sailFrontAndBack.transform.localScale = Vector3.one * currentRatio;
+            sailMiddleFront.transform.localScale = Vector3.one * currentRatio;
+            sailMiddleBack.transform.localScale = Vector3.one * currentRatio;
+
+            yield return new WaitForSecondsRealtime(0.4f);
+        }
+
+        // Shrink for a few seconds
+        while (this.currentRatio != this.shrinkBound)
+        {
+            // Determine the new ratio to use
+            currentRatio = Mathf.MoveTowards(currentRatio, shrinkBound, approachSpeed);
+
+            // Update element
+            sailFrontAndBack.transform.localScale = Vector3.one * currentRatio;
+            sailMiddleFront.transform.localScale = Vector3.one * currentRatio;
+            sailMiddleBack.transform.localScale = Vector3.one * currentRatio;
+
+            yield return new WaitForSecondsRealtime(0.4f);
+        }
+
+        if(currentRatio == shrinkBound)
+        {
+            isPulsing = false;
+        }
     }
 }
+

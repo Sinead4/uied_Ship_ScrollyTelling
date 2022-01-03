7612df8506f37370bf586c45fe5047f6e99e28c4
marcgutzwiller <62597982+marcgutzwiller@users.noreply.github.com>
2022-01-03 17:16:19 +0100

    Center miles-text

--- Assets/Scripts/MilesTextHandling.cs
+++ Assets/Scripts/MilesTextHandling.cs
@@ -17,7 +17,7 @@ public class MilesTextHandling : MonoBehaviour
             currentSpeedText.gameObject.SetActive(true);
         }
         float speed = distanceTraveled / 50 + 2 * distanceTraveled / 217;
-        Vector3 changeZPosition = new Vector3(0, 60, -30);
+        Vector3 changeZPosition = new Vector3(-25, 60, -30);
         currentSpeedText.transform.position = position+changeZPosition;
         currentSpeedText.text = "Das Schiff fährt " + (int)(speed*1.852) + " Knoten  Das entspricht ca. "+ (int)speed + " km/h";
         

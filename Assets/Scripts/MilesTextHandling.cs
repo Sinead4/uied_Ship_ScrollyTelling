c7900935355b37a467da7c3b53a1a71ff8ca1672
marcgutzwiller <62597982+marcgutzwiller@users.noreply.github.com>
2022-01-08 19:41:03 +0100

    Update miles-text

--- Assets/Scripts/MilesTextHandling.cs
+++ Assets/Scripts/MilesTextHandling.cs
@@ -19,7 +19,7 @@ public class MilesTextHandling : MonoBehaviour
         float speed = distanceTraveled / 50 + 2 * distanceTraveled / 217;
         Vector3 changeZPosition = new Vector3(-25, 60, -30);
         currentSpeedText.transform.position = position+changeZPosition;
-        currentSpeedText.text = "Das Schiff fährt " + (int)(speed*1.852) + " Knoten  Das entspricht ca. "+ (int)speed + " km/h";
+        currentSpeedText.text = "Das Schiff fährt " + (int)(speed*1.852)/2 + " Knoten  Das entspricht ca. "+ (int)speed/2 + " km/h";
         
     }
 }
